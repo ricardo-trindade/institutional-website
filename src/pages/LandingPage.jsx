f9c0be684053e0b8e21ace18621b5e5451635876
Ricardo Trindade <ricardotrindade.dev@gmail.com>
2026-05-04 18:52:59 -0300

    add reveal effect

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,16 +4,17 @@ import Specialities from '../components/Specialities'
 import Projects from '../components/Projects'
 import CallToAction from '../components/CallToAction'
 import Footer from '../components/Footer'
+import Reveal from '../components/Reveal'
 
 export default function LandingPage() {
     return (
         <>
             <Header/>
-            <Hero/>
-            <Specialities/>
-            <Projects/>
-            <CallToAction/>
-            <Footer/>
+            <Reveal><Hero/></Reveal>
+            <Reveal delay="0.05s"><Specialities/></Reveal>
+            <Reveal delay="0.05s"><Projects/></Reveal>
+            <Reveal delay="0.05s"><CallToAction/></Reveal>
+            <Reveal><Footer/></Reveal>
         </>
     )
 }
